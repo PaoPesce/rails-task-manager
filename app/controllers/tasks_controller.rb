228759fe252cb8a89166513bf3d442a67ec09556
paola pesce <paojpesce@gmail.com>
2022-09-10 16:20:11 -0300

    final ejercico 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def new
@@ -17,18 +19,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    
   end
 
   def update
-    @task = Task.find(params[:id])
+
     @task.update(task_params)
     redirect_to tasks_path(@task)
   end
-  
+
+  def destroy
+
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
